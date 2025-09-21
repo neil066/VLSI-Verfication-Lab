803c0bbfb352dcf4cbfdf99f6a3b1d47f6c41310
neil066 <163761237+neil066@users.noreply.github.com>
2025-09-21 17:18:20 -0400

    Add files via upload

---
+++ 4-bit-add-hier.v
@@ -0,0 +1,42 @@
+
+// 4-bit Ripple Carry Adder (using 4 Full Adders)
+module adder4 (
+    input  [3:0] a,
+    input  [3:0] b,
+    input        cin,
+    output [3:0] sum,
+    output       cout
+);
+    wire c1, c2, c3;
+
+    full_adder FA0 (.a(a[0]), .b(b[0]), .cin(cin),  .sum(sum[0]), .cout(c1));
+    full_adder FA1 (.a(a[1]), .b(b[1]), .cin(c1),   .sum(sum[1]), .cout(c2));
+    full_adder FA2 (.a(a[2]), .b(b[2]), .cin(c2),   .sum(sum[2]), .cout(c3));
+    full_adder FA3 (.a(a[3]), .b(b[3]), .cin(c3),   .sum(sum[3]), .cout(cout));
+
+endmodule
+
+
+// Half Adder
+module half_adder (
+    input  a,
+    input  b,
+    output sum,
+    output carry
+);
+    assign sum   = a ^ b;
+    assign carry = a & b;
+endmodule
+
+// Full Adder
+module full_adder (
+    input  a,
+    input  b,
+    input  cin,
+    output sum,
+    output cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+
+endmodule
